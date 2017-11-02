84d4e27a2289d463b910af90dfb1abe897f7cd77
mauro003241 <clashnuevo03@gmail.com>
2017-11-02 10:21:55 -0300

    se creo la clase cliente y se creo un objeto mostrando el nombre

--- test_poo/src/main/Main.java
+++ test_poo/src/main/Main.java
@@ -9,6 +9,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hola mundo ");
+        
+        cliente cli = new cliente(1,  "mauro");
+        System.out.println(cli.getNombre());
     }
     
 }
---
+++ test_poo/src/main/cliente.java
@@ -0,0 +1,38 @@
+
+package main;
+
+/**
+ *
+ * @author mauro
+ */
+public class cliente {
+
+    private int id;
+    private String nombre;
+
+    public cliente(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+}
+
+    
+
